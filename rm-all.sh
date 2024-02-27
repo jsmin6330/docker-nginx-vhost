164f6c5a0cce5f6f882227797431202ea2da9a62
jsmin6330 <jsmin6330@gmail.com>
2024-02-27 11:19:21 +0900

    update rm-all.sh

--- rm-all.sh
+++ rm-all.sh
@@ -2,4 +2,6 @@ figlet remove serv-a , serv-b , lb
 docker stop serv-a serv-b lb
 docker rm serv-a serv-b lb
 docker rmi serv-a:1 serv-b:1 lb:1
-docker network rm  r-net
+docker rmi soominjeong/serv-a:1
+docker rmi soominjeong/serv-b:1
+docker network rm r-net
